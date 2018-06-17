e23a91e6e80c425b0b75f4a17f36b4e2441a9ea8
Matheus <matheusteixeira2705@gmail.com>
2018-06-17 19:10:49 -0300

    FizzBuzz

--- Q1/fizzbizz.c
+++ Q1/fizzbizz.c
@@ -11,8 +11,18 @@ void fizzBizz(int start, int end)
 {
     printf("\nFizzBizz::START: %d, %d\n", start, end);
     
-    // TODO: Your code here!
-    // Ex.: printf("Fizz\n");
+    while (start <= end) {
+		if (start % 3 == 0 && start % 5 == 0) {
+			printf("FizzBuzz\n");
+		} else if (start % 3 == 0) {
+			printf("Fizz\n");
+		} else if (start % 5 == 0) {
+			printf("Buzz\n");
+		} else {
+			printf("%d\n", start);
+		}
+		start++;
+	}
     
     printf("\nFizzBizz::END\n");
 }
